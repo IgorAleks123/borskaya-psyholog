50659569332cc3dec4ea242389d4071d3878b414
IgorAleks123 <98655162+IgorAleks123@users.noreply.github.com>
2024-01-09 20:08:17 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".mySwiper", {
+      effect: "cards",
+      grabCursor: true,
+    });
+
+var mySwiper1 = new Swiper(".swiper-container", {
+  direction: "vertical",
+  loop: true,
+  pagination: ".swiper-pagination2",
+  grabCursor: true,
+  speed: 1000,
+  paginationClickable: true,
+  parallax: true,
+  autoplay: false,
+  effect: "slide",
+  mousewheelControl: 1
+});
+
+
+
